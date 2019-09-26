1669233bd8ba73211e413c43031961820daeaca1
James <jordunjames@gmail.com>
2019-09-26 12:51:10 +0100

    refactored login handlers to arrow functions and added local run script for local machine

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -16,17 +16,17 @@ export class LoginPage extends Component {
             password: '',
             submitted: false
         };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e) {
-       
+    handleChange = (e) => {
+        // const { name } = e.target;
+        // this.setState({ [name]: e.target.value })
     }
 
-    handleSubmit(e) {
-        
+    handleSubmit = (e) => {
+        // e.preventDefault();
+        // const { username, password } = this.state;
+        // console.log({ username, password })
     }
 
     render() {
@@ -34,17 +34,17 @@ export class LoginPage extends Component {
         return (
             <div className="col-md-6 col-md-offset-3">
                 <h2>Login</h2>
-                <form name="form">
+                <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
                         <label htmlFor="username">Username</label>
-                        <input type="text" className="form-control username" name="username" />
+                        <input type="text" className="form-control username" name="username" onChange={this.handleChange} value={username} />
                         {submitted && !username &&
                             <div className="help-block">Username is required</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                         <label htmlFor="password">Password</label>
-                        <input type="password" className="form-control" name="password"/>
+                        <input type="password" className="form-control" name="password" onChange={this.handleChange} value={password} />
                         {submitted && !password &&
                             <div className="help-block">Password is required</div>
                         }
